98d7ac9c224ac8dae8d874b34bdc33119cc9aa23
MarcoPires <marcofspires@gmail.com>
2015-12-22 22:26:32 +0000

    P3 project add "Restaurant app" with changes to new domain

---
+++ restaurantApp/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from restaurantApp import config
+from restaurantApp.persistence import add_default_data
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = config.data["upload_folder"]
+
+
+#from restaurantApp.router import views, jsonEndpoints, xmlEndpoints
+
+from restaurantApp.router import jsonEndpoints, xmlEndpoints
+from restaurantApp.router import views
+
+log = 'test'
